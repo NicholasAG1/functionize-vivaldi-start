1fb0e407c114b4f3483ebbbe14f1c51169a9c92a
NicholasAG1 <70723112+NicholasAG1@users.noreply.github.com>
2020-09-09 15:17:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 // Vivald JS Example
 
 // When a Season Button is Clicked
-document.getElementById('summerBtn').addEventListener('click', setSeason('Summer', '#1BA848' ));
-document.getElementById('autumnBtn').addEventListener('click', setSeason('Autumn', '#FE6732'));
-document.getElementById('winterBtn').addEventListener('click', setSeason('Winter', '#1C64B9'));
-document.getElementById('springBtn').addEventListener('click', setSeason('Spring', '#0E94D1')); 
+document.getElementById('springbtn').addEventListener('click', setSpring);
+document.getElementById('summerbtn').addEventListener('click', setSummer);
+document.getElementById('autumnbtn').addEventListener('click', setAutumn);
+document.getElementById('winterBtn').addEventListener('click', setWinter);
+
+function setSpring(){
+    setSeason('Spring', '#0E94D1')
+}
+
+function setSummer(){
+    setSeason('Summer', '#1BA848')
+}
+
+function setAutumn(){
+    setSeason('Autumn', '#FE6732' )
+}
+
+function setWinter(){
+    setSeason ('winter', '#1C64B9' )
+}
 
 // Event Functions
 function setSeason(name, color) {
@@ -15,7 +31,7 @@ function setSeason(name, color) {
     document.getElementById('main-img').src = 'images/' + name.tolowercase() + '.jpg';
      
     // - change page background color
-    document.body.style.backgroundColor = 'color';
+    document.body.style.backgroundColor = color;
      
     // - change audio source
     document.getElementById('song').src = 'songs/vivaldi-' + name.tolowercase() + '.mp3';
